0a89acaf7b7f403cd3416f615548221158cf68b0
chris <acomputerdog@gmail.com>
2017-07-02 20:31:26 -0400

    Implement some commands

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ MagicWells:
 A bukkit plugin that adds magic wells to the game
 ---
 MagicWells adds naturally-generating "magic wells"
-with special powers.
+with special powers.  These wells can currently be used
+to teleport or wish for items.  Wells can be claimed by
+a player, who must invest rare resources to protect 
+his or her claim.
 
-Requires HSQLDB library to compile.
+* Requires HSQLDB (HyperSQL) library to compile.
+* External database support is planned but not yet implemented.
--- src/net/acomputerdog/magicwells/MWCommandHandler.java
+++ src/net/acomputerdog/magicwells/MWCommandHandler.java
@@ -13,11 +13,92 @@ public class MWCommandHandler {
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         switch (command.getName()) {
-
+            case "mwreload":
+                handleReload(sender);
+                break;
+            case "mwlist":
+                handleList(sender);
+                break;
+            case "mwport":
+                handlePort(sender, args);
+                break;
+            case "mwrecheck":
+                handleRecheck(sender);
+                break;
+            case "mwinfo":
+                handleInfo(sender, args);
+                break;
+            case "mwrepair":
+                handleRepair(sender, args);
+                break;
+            case "mwrename":
+                handleRename(sender, args);
+                break;
             default:
                 sendRed(sender, "Internal error: that command was not recognised by MagicWells.  Please report this!");
-                return true;
         }
+        return true;
+    }
+
+    private boolean checkPerms(CommandSender sender, String perm) {
+        if (!sender.hasPermission("magicwells.command." + perm)) {
+            sendRed(sender, "You do not have permission to use that command.");
+            return false;
+        }
+        return true;
+    }
+
+    private void handleReload(CommandSender sender) {
+        if (checkPerms(sender, "mwreload")) {
+            sendAqua(sender, "Reloading...");
+            plugin.onDisable();
+            plugin.onEnable();
+            sendAqua(sender, "Done.");
+        }
+    }
+
+    private void handleList(CommandSender sender) {
+        if (checkPerms(sender, "mwlist")) {
+            sendYellow(sender, "Sorry, that command is not implemented.");
+        }
+    }
+
+    private void handlePort(CommandSender sender, String[] args) {
+        if (checkPerms(sender, "mwport")) {
+            sendYellow(sender, "Sorry, that command is not implemented.");
+        }
+    }
+
+    private void handleRecheck(CommandSender sender) {
+        if (checkPerms(sender, "mwrecheck")) {
+            sendYellow(sender, "Sorry, that command is not implemented.");
+        }
+    }
+
+    private void handleInfo(CommandSender sender, String[] args) {
+        if (checkPerms(sender, "mwinfo")) {
+            sendYellow(sender, "Sorry, that command is not implemented.");
+        }
+    }
+
+    private void handleRepair(CommandSender sender, String[] args) {
+        if (checkPerms(sender, "mwrepair")) {
+            sendYellow(sender, "Sorry, that command is not implemented.");
+        }
+    }
+
+    private void handleRename(CommandSender sender, String[] args) {
+        if (checkPerms(sender, "mwrename")) {
+            sendYellow(sender, "Sorry, that command is not implemented.");
+        }
+    }
+
+    private static void sendYellow(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.YELLOW + message);
+    }
+
+    private static void sendAqua(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.AQUA + message);
     }
 
     private static void sendRed(CommandSender sender, String message) {
